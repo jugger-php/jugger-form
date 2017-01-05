7ab8fbc711eaceb5fae39864ad251be001463f69
irpsv <i.rpsv@live.com>
2017-01-05 23:39:39 +0500

    update Form

--- src/Form.php
+++ src/Form.php
@@ -4,16 +4,21 @@ namespace jugger\form;
 
 use jugger\model\Model;
 
-class Form extends Model
+abstract class Form extends Model
 {
+    public static function getName()
+    {
+        return (new \ReflectionClass(get_called_class()))->getShortName();
+    }
+
     public static function load(array $request)
     {
         $form = new static();
-        $form->setValues($request[$form->name] ?? $request);
+        $form->setValues($request[$form::getName()] ?? $request);
         return $form;
     }
 
-    public static function getLabel(string $name)
+    public static function getLabel(string $name): string
     {
         return static::getLabels()[$name] ?? $name;
     }
@@ -23,9 +28,9 @@ class Form extends Model
         return [];
     }
 
-    public static function getHint(string $name)
+    public static function getHint(string $name): string
     {
-        return static::getHints()[$name] ?? null;
+        return static::getHints()[$name] ?? "";
     }
 
     public static function getHints(): array
@@ -33,18 +38,18 @@ class Form extends Model
         return [];
     }
 
-    public function submit()
+    public function submit(): array
     {
         if (!$this->validate()) {
-            return false;
+            return $this->getErrors();
         }
 
         $result = $this->handle();
         if ($result->isSuccess()) {
-            return true;
+            return [];
         }
         else {
-            throw $result->getException();
+            return [$result->getMessage()];
         }
     }
 }
